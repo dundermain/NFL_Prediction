0bfd4ebd454681d0adec7e7e3196f8177f981e91
dundermain <sachin68834@gmail.com>
2025-01-01 00:29:27 +0530

    initial commits

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from nfl-prediction!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ nfl_prediciton_assistant/src/nfl_prediciton_assistant/main.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from nfl_prediciton_assistant.crew import NflPredicitonAssistant
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'AI LLMs'
+    }
+    NflPredicitonAssistant().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        NflPredicitonAssistant().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        NflPredicitonAssistant().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        NflPredicitonAssistant().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
